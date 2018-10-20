975e960d7f1a47c7be2def1ea2977b645151e1e8
Muhand Jumah <muhandjumah@gmail.com>
2018-10-20 15:39:41 -0400

    Capture the video

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import numpy as np
 import cv2
+
+# Capture the frames of the video
+cap = cv2.VideoCapture('input.mov')
+
+# As long as thee capture is still open  then the loop will run
+while(cap.isOpened()):
+	# Read the frames of the video
+    ret, frame = cap.read()
+
+    # Show the video in a frame
+    cv2.imshow('frame',frame)
+
+    # Break out of the loop on quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# De-allocate any associated memory usage
+cap.release()
+cv2.destroyAllWindows()
